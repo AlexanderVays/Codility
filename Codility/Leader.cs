d62a8d86728bbb36b40fb97c9267a6ed0c4769c7
Alexander Vays <vays.alex@gmail.com>
2020-07-02 13:43:02 -0700

    Initial Commit

---
+++ Codility/Leader.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codility
+{
+    class Leader
+    {
+        public int solution(int[] A)
+        {
+            int n = A.Length;
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] firstPart = new int[i + 1];
+                int[] secondPart = new int[n - i - 1];
+                int firstTop = 0, secondTop = 0;
+                Dictionary<int, int> first = new Dictionary<int, int>();
+                Dictionary<int, int> second = new Dictionary<int, int>();
+
+                for (int j = 0; j < firstPart.Length; j++)
+                {
+                    if (!first.ContainsKey(A[j]))
+                    {
+                        first.Add(A[j], 1);
+                    }
+                    else
+                    {
+                        first[A[j]]++;
+                    }
+                }
+
+                if (first.Count > 0)
+                {
+                    firstTop = first.OrderByDescending(pair => pair.Value).Take(1).First().Value;
+                }
+
+                if (firstTop > first.Count / 2 || i == 0)
+                {
+                    for (int j = 0; j < firstPart.Length; j++)
+                    {
+                        if (!second.ContainsKey(A[j]))
+                        {
+                            second.Add(A[j], 1);
+                        }
+                        else
+                        {
+                            second[A[j]]++;
+                        }
+                    }
+                }
+                if (second.Count > 0)
+                {
+                    secondTop = second.OrderByDescending(pair => pair.Value).Take(1).First().Value;
+                }
+
+                if (secondTop > second.Count / 2 || i == n - 1)
+                {
+                    if (firstTop == secondTop)
+                    {
+                        if (!result.ContainsKey(firstTop))
+                        {
+                            result.Add(firstTop, 1);
+                        }
+                        else
+                        {
+                            result[firstTop]++;
+                        }
+                    }
+                }
+            }
+
+            if (result.Count > 0)
+            {
+                return result.OrderByDescending(pair => pair.Value).Take(1).First().Value;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Codility/Sorting.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Codility
+{
+    public class Sorting
+    {
+        public static int NumberOfDiscIntersections(int[] A) 
+        {
+            // The force forward solution which is O(N^2) complexity
+            /*
+            int intersections = 0;
+            int n = A.Length;
+            for (long i = 0; i < n - 1; i++)
+            {
+                for (long j = i + 1; j < n; j++)
+                {
+                    if (i + A[i] >= j - A[j] && i - A[i] <= j + A[j])
+                    {
+                        if (intersections == 10E6)
+                        {
+                            return -1;
+                        }
+                        else
+                        {
+                            intersections++;
+                        }
+                    }
+                }
+            }
+            return intersections; */
+
+            // O(N*log(N)) complexity by using sorting
+            long n = A.Length;
+            if (n < 2) 
+            {
+                return 0;
+            }
+
+            long intersecs = n * (n - 1) / 2; // max possible intersects between n discs: (n-1)+(n-2)+...3+2+1 = n*(n-1)/2
+            long[] highValue = new long[n]; // high x values
+            long[] lowValue = new long[n]; // low x values
+            for (long i = 0; i < n; i++) 
+            {
+                highValue[i] = i + A[i];
+                lowValue[i] = i - A[i];
+            }
+            Array.Sort(highValue);
+            Array.Sort(lowValue);
+
+            int j = 0; // initialize inner iteration only once
+            for (int i = 0; i < n; i++) 
+            {
+                for (; j < n; j++) 
+                {
+                    if (lowValue[j] > highValue[i]) // discs dont intersect when low > high (x values)
+                    {
+                        intersecs -= n - j; // remove all discs where low > high
+                        break;
+                    }
+                }
+
+                if (j == n) // no more low values greater than high values
+                {
+                    break;
+                }
+            }
+
+            if (intersecs > 10E6) // if number of intersects greater than 10,000,000
+            {
+                return -1;
+            }
+            return (int) intersecs;
+        }
+    }
+}
